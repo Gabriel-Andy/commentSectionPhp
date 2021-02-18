cf12e55da669e962b6044753841dc270f5729f50
gabriel andy fonogit config --global user.email fonoandy <fonoandy@gmail.com>
2021-02-18 12:51:35 -0500

    I am learning how to create comment section

---
+++ index.php
@@ -0,0 +1,29 @@
+<?php
+date_default_timezone_set('Europe/Copenhagen');
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel = stylesheet type = 'text/css' href = 'style.css'>
+</head>
+<body>
+    <video width = "320"  height = "240" controls>
+    <source src = "movie.mp4" type = "video/mp4">
+    <source src = "movie.ogg" type = "video/ogg">
+    your browser doesn not support the video tag.
+    </video>
+    <?php
+echo "<form>
+          <input type='hidden ' name = 'uid' value = 'Anonymous'>
+    <input type='hidden ' name = 'date' value = '".date('Y-m-d H:i:s')."'><br>
+    <textarea name = 'message' ></textarea> <br>
+     <button name = 'submit' type = 'submit'>Comment</button>
+      </form>";
+      ?>
+    
+</body>
+</html>
